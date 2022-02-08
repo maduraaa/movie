9bc989d473a2f0a066cfcf9ce05f792eeebe631b
levan.madurashvili <55601009+maduraaa@users.noreply.github.com>
2022-02-08 22:29:24 +0400

    add new movie project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <Wrapper>
+      dashboard
+    </Wrapper>
+  );
+}
+
+
+const Wrapper = styled.div`
+ 
+`;
+
+export default App;
